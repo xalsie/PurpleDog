04384d6708005c185064d10f846d7992822e40bc
Florent <148872635+Froggys974@users.noreply.github.com>
2025-12-11 17:33:23 +0100

    Change CORS origin from localhost:3001 to 3000

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.enableCors({
-        origin: env.FRONT_URL || 'http://localhost:3001',
+        origin: env.FRONT_URL || 'http://localhost:3000',
         credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     });
